db54211dc4dad41285609e044889100c4ac59f47
Kaleeur Rahman <rahman@pelicancube.com>
2022-09-21 23:08:32 +0530

    login fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,106 +4,100 @@ import React from "react";
 import './Login.css';
 import { Link } from "react-router-dom";
 
-import  image  from "../../imgs/background.png";
- 
+import image from "../../imgs/background.png";
+
 
 const Login = (props) => {
 
-    const [login,toLog] = React.useState(false);
-    const [username,SetUsername] = React.useState([]);
-    const [password,SetPassword] = React.useState([]);
-    
+    const [login, toLog] = React.useState(false);
+    const [username, SetUsername] = React.useState([]);
+    const [password, SetPassword] = React.useState([]);
 
-    const loginTo = (event) =>{
+    const loginTo = (event) => {
         event.preventDefault();
-        if(username==="hexa" && password==="#HexamTeam99"){
+        if (username === "hexa" && password === "#HexamTeam99") {
             toLog(true);
             console.log("submitted");
             props.changeState(true);
-             
-            
-        }else{
+
+
+        } else {
             alert("Username or Password is Wrong...!");
-        } 
+        }
     }
 
-    
+    return (
+        // <div className="art">
+        <div >
+                <div className="fontname"><center>Login To Admin Page</center></div>
+                <div class="row">
 
-    return(<div className="art">
-        <Link to="/login">
-        <div className="fontname"><center>Login To Admin Page</center></div>
-        {/* div table */}
-        <div class="row">
 
-        
-        <div class="col-sm-5">
-        <div className="Img">
-        
+                    <div class="col-sm-5">
+                        <div className="Img">
 
-        
-        <div  style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat" ,backgroundSize:"contain", height:480,width:580 }}>
-        </div>
-        </div>
-        </div>
-        
-        {/* table */}
-        <div class="col-sm-4" >
-        <center>
-        
-        
-        <form onSubmit={loginTo} className="al">
-            
-            <table className="tbl">
-                <tr className="spaceUnder">   
-                    <td className="td">Enter Username</td>             
-                    <td>
-                        <TextField                    
-                        label="Username"                        
-                        onChange={(e)=>{
-                            SetUsername(e.target.value);
-                        }}
-                        />
-                    </td> 
-                </tr>
-                <tr>
-                    <td><a> </a></td>
-                    <td><a> </a></td>
-                </tr>
-                <tr className="spaceUnder">
-                    <td className="td">Enter password</td>
-                    <td>
-                        <TextField 
-                        type="Password"                        
-                        label="Password"                        
-                        onChange={(e)=>{
-                            SetPassword(e.target.value);
-                        }}
-                        />
-                    </td>
-                </tr>
-            </table>
-            <div>
-                <center>
-                <Button 
-                size="large"
-                fullWidth={true}
-                type="submit"
-                color="inherit"
-                >Login</Button>
-                </center>
-            </div>              
-                
-            
-            
-        </form>
-        </center>
-        </div>
-        </div>
-        </Link>
+                            <div style={{ backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", height: 480, width: 580 }}>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* table */}
+                    <div class="col-sm-4" >
+                        <center>
+
+
+                            <form onSubmit={loginTo} className="al">
+
+                                <table className="tbl">
+                                    <tr className="spaceUnder">
+                                        <td className="td">Enter Username</td>
+                                        <td>
+                                            <TextField
+                                                label="Username"
+                                                onChange={(e) => {
+                                                    SetUsername(e.target.value);
+                                                }}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><a> </a></td>
+                                        <td><a> </a></td>
+                                    </tr>
+                                    <tr className="spaceUnder">
+                                        <td className="td">Enter password</td>
+                                        <td>
+                                            <TextField
+                                                type="Password"
+                                                label="Password"
+                                                onChange={(e) => {
+                                                    SetPassword(e.target.value);
+                                                }}
+                                            />
+                                        </td>
+                                    </tr>
+                                </table>
+                                <div>
+                                    <center>
+                                        <Button
+                                            size="large"
+                                            fullWidth={true}
+                                            type="submit"
+                                            color="inherit"
+                                        >Login</Button>
+                                    </center>
+                                </div>
+
+
+
+                            </form>
+                        </center>
+                    </div>
+                </div>
         </div>
     );
-   
-    
+
+
 }
 
 export default Login
